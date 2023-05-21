4d77d8735d1fea1b5c3e4042e6f13350eb469cfa
kartik-ahire <119925465+kartik-ahire@users.noreply.github.com>
2023-05-21 08:44:23 +0530

    Add files via upload

---
+++ Lecture-003 If-Else, While loop & Lots of Patterns/whileLoop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cout<<"\n Enter number: ";
+    cin>>n;
+    int i = 1;
+    while(i<=n) 
+    {
+        cout<<i<<" ";
+        i = i+1;
+        
+    }
+}
